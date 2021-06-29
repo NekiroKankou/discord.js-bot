2e4142c39599aabf8d835c3bd5765c60d9e99c97
KanakaID <66167985+KanakaID@users.noreply.github.com>
2021-06-29 19:13:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -41,4 +41,49 @@ Channels : ${client.channels.cache.size}
 		client.user.setStatus('idle');
 	});
 
+client.on('message', async message => {
+	if (message.author.bot) return;
+
+	if (!message.guild) return;
+	
+	if(message.channel.id === '825987625848274954') {
+	  if(message.content === 'Pagi') {
+	    message.channel.send('Pagi Juga <:NayuSmile:792207278312783882>')
+	  }
+	}
+	//Message
+	if (message.content === `<@${client.user.id}>`) {
+		const kawai = client.emojis.cache.get('801780468554006609');
+		const mention = new discord.MessageEmbed()
+			.setTitle(`Koniciwa`)
+			.setDescription(
+				`${kawai} Hello Oniichan, My Name is ${
+					client.user.username
+				} And My Prefix is ${client.prefix}`
+			)
+			.setColor('BLUE');
+		message.channel.send(mention);
+	}
+	
+
+	//Commands
+	if (!message.content.startsWith(prefix)) return;
+
+	if (!message.member)
+		message.member = await message.guild.fetchMember(message);
+
+	const args = message.content
+		.slice(prefix.length)
+		.trim()
+		.split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	if (cmd.length === 0) return;
+
+	let command = client.commands.get(cmd);
+	
+	if (!command) command = client.commands.get(client.aliases.get(cmd));
+	if (command) command.run(client, message, args);
+});
+
 client.login(token);
