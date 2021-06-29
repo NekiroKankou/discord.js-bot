a5aa2859f19b8374b939ac71699f5c26c6cc4cf6
KanakaID <66167985+KanakaID@users.noreply.github.com>
2021-06-29 19:11:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,34 @@ const snek = require('node-superfetch');
 
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
-client.queue = new Map();
 client.prefix = prefix;
-client.hastebin = async text => {
-	const { body } = await snek
-		.post('https://bin-clientdev.glitch.me/documents')
-		.send(text);
-	return `https://bin-clientdev.glitch.me/${body.key}`;
-};
 
 ['command'].forEach(handler => {
 	require(`./handlers/${handler}`)(client);
 });
-	
+
+client.on('ready', () => {
+		console.log(`
+◊═══════════════════════════◊
+${client.user.username} Siap Di Gunakan Oniichan.
+Name : ${client.user.tag}
+Server : ${client.guilds.cache.size}
+Member : ${client.users.cache.size}
+Channels : ${client.channels.cache.size}
+◊═══════════════════════════◊
+`);
+	});
+
+	client.on('ready', () => {
+		setInterval(() => {
+			var member = client.users.cache.size;
+			let status = `🛡️ ${member} Oniichan | prefix "${prefix}"`;
+			client.user.setActivity(status, { type: 'WATCHING' });
+		}, 50000);
+	});
+
+	client.on('ready', () => {
+		client.user.setStatus('idle');
+	});
+
 client.login(token);
