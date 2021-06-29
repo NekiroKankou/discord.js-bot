093438cbc14407b1f87d6c48544ef3af65f110b3
KanakaID <66167985+KanakaID@users.noreply.github.com>
2021-06-29 19:26:02 +0700

    Create example.js

---
+++ commands/info/example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name:'example',
+    description:'example command',
+    aliases:['ex']
+    run: async (client, message, args) => {
+       message.channel.send('Masuk')
+}}
